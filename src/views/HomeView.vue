22d8f32d61c70637f96741a9a0cbf46ea331f08d
JakubWrzesniak <jakub.wrzesniak@outlook.com>
2022-04-05 16:31:24 +0200

    Fix geting books

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <books-table msg="Welcome to Your Vue.js App"/>
+    <books-table :booksSource="books"  msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
@@ -13,6 +13,26 @@ export default {
   name: 'HomeView',
   components: {
     BooksTable
+  },
+  data(){
+    return {
+      books: [],
+    }
+  },
+  methods: {
+    async getBooks(){
+      try{
+        const response = await fetch("http://localhost:8080/get/books")
+        const data = await response.json()
+        this.books = data
+        console.log(this.books)
+      } catch(error){
+        console.error(error)
+      }
+    },
+  },
+  mounted(){
+    this.getBooks()
   }
 }
 </script>
